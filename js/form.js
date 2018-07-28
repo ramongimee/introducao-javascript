01485509af68a717da94a31e3e0293af93ded65e
Ramon <ramongimee@hotmail.com>
2018-07-28 17:32:22 -0300

    Validando o restante dos campos

--- js/form.js
+++ js/form.js
@@ -6,8 +6,10 @@ botaoAdicionar.addEventListener("click", function(event) {
     var paciente = obtemPacienteDoFormulario(form);
     var pacienteTr = montaTr(paciente);
 
-    if (!validaPaciente(paciente)) {
-        console.log("Paciente inválido");
+    var erros = validaPaciente(paciente);
+    console.log(erros);
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
         return;
     }
 
@@ -55,9 +57,26 @@ function montaTd(dado,classe){
 }
 
 function validaPaciente(paciente) {
-    if (validaPeso(paciente.peso)) {
-        return true;
-    } else {
-        return false;
-    }
+
+  var erros = [];
+
+    if (paciente.nome.length == 0) erros.push("O nome não pode ser em branco");
+    if (paciente.gordura.length == 0) erros.push("A gordura não pode ser em branco");
+    if (paciente.peso.length == 0) erros.push("O peso não pode ser em branco");
+    if (paciente.altura.length == 0) erros.push("A altura não pode ser em branco");
+    if (!validaPeso(paciente.peso)) erros.push("Peso é inválido");
+    if (!validaAltura(paciente.altura)) erros.push("Altura é inválida");
+
+    return erros;
+}
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+     ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
